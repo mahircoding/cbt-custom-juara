69a708207e4a85d24ced22f98f9e15632955dbd5
Your Name <you@example.com>
2026-04-13 16:03:55 +0700

    fix: explicit public path for Vite manifest on server

--- computer-assisted-test/bootstrap/app.php
+++ computer-assisted-test/bootstrap/app.php
@@ -15,6 +15,12 @@ $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
+$app->usePublicPath(
+    file_exists($app->basePath('../public_html'))
+        ? $app->basePath('../public_html')
+        : $app->basePath('../')
+);
+
 /*
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
